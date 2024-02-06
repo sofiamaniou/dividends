0f7e1b8223942e7108e6b690a7e92efa01006784
Sofia Maniou <maniou_s@hotmail.com>
2024-02-06 14:09:19 +0200

    Wrap root component with error boundary

---
+++ src/error/ErrorBoundaryState.ts
@@ -0,0 +1,10 @@
+/**
+ * The Error boundary state.
+ */
+export interface ErrorBoundaryState {
+    /**
+     * Current error or undefined if none.
+     */
+    readonly error: Error | undefined;
+  }
+  
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { ErrorBoundary } from './error/ErrorBoundary.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ErrorBoundary errorComponent={<h2>Oops! Something went wrong.</h2>}>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
 )
